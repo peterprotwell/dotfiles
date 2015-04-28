787802e33bad3273c28ea48995ef8d1be4bccf2c
Mike Nichols <mykphyre@yahoo.com>
2015-04-28 10:53:54 -0500

    Adding flux to brew cask install stuff.

--- init-mac.sh
+++ init-mac.sh
@@ -25,7 +25,7 @@ done
 
 brew install caskroom/cask/brew-cask
 
-apps="emacs firefox gitx google-chrome iterm2 macdown menumeters\
+apps="emacs firefox flux gitx google-chrome iterm2 macdown menumeters\
  openoffice paintbrush scroll-reverser sizeup skype vlc"
 
 for app in $apps; do
