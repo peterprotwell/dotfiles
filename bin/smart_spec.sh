2a24ef8ecbc0bc02d2b6737f391fb6a728065e72
Mike Nichols <mykphyre@yahoo.com>
2017-05-14 12:15:11 -0500

    Filter out non-tests in smart_spec.sh

--- bin/smart_spec.sh
+++ bin/smart_spec.sh
@@ -5,7 +5,13 @@ IFS=$'\n\t'
 forkpoint="$(git merge-base --fork-point master)"
 changed_files="$(git diff --name-only "$forkpoint")"
 
-echo "Running tests for changed files:
-$changed_files"
+# Filter out non-spec files
+spec_files="$(echo "$changed_files" | grep '_spec.rb')"
 
-bundle exec rspec $changed_files
+# Match against spec files for modified implementation files
+# non_spec_files=$(echo "$changed_files" | grep --invert-match '_spec.rb')
+
+echo "Running tests for changed files:
+$spec_files"
+
+bundle exec rspec $spec_files
