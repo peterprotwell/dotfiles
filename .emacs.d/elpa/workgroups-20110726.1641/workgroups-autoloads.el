f3a1a41694ca8e0912c890e017061d44a7763eb7
Mike Nichols <mykphyre@yahoo.com>
2018-10-02 13:39:48 -0500

    Update all emacs packages

---
+++ .emacs.d/elpa/workgroups-20110726.1641/workgroups-autoloads.el
@@ -0,0 +1,15 @@
+;;; workgroups-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("workgroups.el") (23475 47516 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; workgroups-autoloads.el ends here
