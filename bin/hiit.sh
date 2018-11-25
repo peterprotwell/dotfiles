373ba1183a6e02fb464f6d9d4b835b48583425d8
Mike Nichols <mykphyre@yahoo.com>
2018-11-25 10:52:24 -0600

    Add hiit.sh to dotfiles

---
+++ bin/hiit.sh
@@ -0,0 +1,35 @@
+sleep 3
+say 'Ready in 3'
+sleep 1
+say '2'
+sleep 1
+say '1'
+sleep 1
+
+for i in 1 2 3; do
+  say 'high knees'
+  sleep 30
+  say 'push ups'
+  sleep 30
+  say 'squats'
+  sleep 30
+  say 'sit ups'
+  sleep 30
+  say 'mountain climbers'
+  sleep 30
+  say 'elbows to hands plank'
+  sleep 30
+  say 'alternating lunges'
+  sleep 30
+  say 'dips'
+  sleep 30
+  say 'burpees'
+  sleep 30
+  say 'leg raise circles'
+  sleep 30
+  say 'one minute rest'
+  sleep 60
+done
+
+say 'duh duh duh da duh, dat du daaaahh'
+say 'a winner is you'
